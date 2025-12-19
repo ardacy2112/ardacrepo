321c9dd97a10e5b2bddaa2720a1014303a13220f
ardacy2112 <138930253+ardacy2112@users.noreply.github.com>
2025-12-19 21:13:12 +0300

    Restore Gradle files, update build.gradle.kts with correct repo URL, and restore APK URLs in plugins.json

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,7 +36,7 @@ subprojects {
 
     cloudstream {
         // when running through github workflow, GITHUB_REPOSITORY should contain current repository name
-        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/ardac/ardacrepo")
+        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/ardacy2112/ardacrepo")
 
         authors = listOf("ardac")
     }
