766908f736c03310de92f0df6287e4773d7eb334
ardacy2112 <138930253+ardacy2112@users.noreply.github.com>
2025-12-19 21:41:15 +0300

    Fix CloudStream dependency to correct JitPack format

--- build.gradle.kts
+++ build.gradle.kts
@@ -74,7 +74,7 @@ subprojects {
         val implementation by configurations
 
         // Stubs for all Cloudstream classes
-        cloudstream("com.lagradost:cloudstream3:3.4.4")
+        cloudstream("com.github.recloudstream:cloudstream:3.4.4")
 
         // these dependencies can include any of those which are added by the app,
         // but you dont need to include any of them if you dont need them
